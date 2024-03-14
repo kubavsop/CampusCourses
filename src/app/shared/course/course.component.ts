2cc718efee7cc1251e858b677767e03e76b68f00
maksim <mazhur04@gmail.com>
2024-03-14 18:57:47 +0700

    Fix courses

--- src/app/shared/course/course.component.ts
+++ src/app/shared/course/course.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
-import {NgIf} from "@angular/common";
+import {NgClass, NgIf} from "@angular/common";
 import {CourseDto} from "../../core/models/dtos/course-dto";
 import {Semester} from "../../core/models/enums/semester";
 import {CourseStatus} from "../../core/models/enums/course-statuses";
@@ -12,14 +12,35 @@ import {CourseStatus} from "../../core/models/enums/course-statuses";
   imports: [
     MatButtonModule,
     MatCardModule,
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
   @Input({required: true}) courseDto: CourseDto
-  statusColor: string = "black"
+  statusColor: string
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    switch (this.courseDto.status) {
+      case CourseStatus.Created:
+        this.statusColor = "#ff4081"
+        break;
+      case CourseStatus.OpenForAssigning:
+        this.statusColor = "#8BC34A";
+        break;
+      case CourseStatus.Started:
+        this.statusColor = "#3f51b5"
+        break;
+      case CourseStatus.Finished:
+        this.statusColor = "#f44336"
+        break;
+    }
+  }
 
   getSemesterName(semester: Semester): string {
     switch (semester) {
@@ -35,16 +56,12 @@ export class CourseComponent {
   getCourseStatusName(courseStatus: CourseStatus): string {
     switch (courseStatus) {
       case CourseStatus.Created:
-        this.statusColor = "#ff4081"
         return "Создан";
       case CourseStatus.OpenForAssigning:
-        this.statusColor = "#8BC34A";
         return "Открыт для записи";
       case CourseStatus.Started:
-        this.statusColor = "#3f51b5"
         return "В процессе обучения";
       case CourseStatus.Finished:
-        this.statusColor = "#f44336"
         return "Закрыт";
       default:
         return "Неизвестный";
